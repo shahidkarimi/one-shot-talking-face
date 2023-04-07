4967d9c5aa9e93900b6f13d661755fd014099e80
Shahid Karimi <karimi.shahid@gmail.com>
2023-04-08 01:44:24 +0500

    uploadian

--- app.py
+++ app.py
@@ -68,8 +68,9 @@ def generate_video():
     ts = int(time.time())
     
     file = request.files['audio']
+    voice_id = request.form['voice_id']
     # save the input files to disk
-    image_path = 'examples/image.png'
+    image_path = f'examples/{voice_id}.png'
     audio_path = f'examples/{ts}.wav'
     file.save(audio_path) #'examples/obama2.wav'
     
